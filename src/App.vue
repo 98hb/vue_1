bc9a1b25dd5dc44d6d1b98b1289b087990faaf52
‘98hb <w17339377595@163.com>
2022-02-12 17:29:36 +0800

    组件缓存

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,19 @@
   <div>
     <h1>1. 动态dynamic组件使用</h1>
     <UseDynamic></UseDynamic>
+    <hr />
+    <h1>2. 组件的缓存</h1>
+    <UseDynamic2></UseDynamic2>
   </div>
 </template>
 
 <script>
 import UseDynamic from "./views/01_UseDynamic.vue";
+import UseDynamic2 from "./views/02_UseDynamic.vue";
 export default {
   components: {
     UseDynamic,
+    UseDynamic2,
   },
 };
 </script>
---
+++ src/components/02/UserName.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <span>用户名:</span>
+      <input type="text" />
+    </div>
+    <div>
+      <span>密码:</span>
+      <input type="text" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log("02-UserName-创建");
+  },
+  destroyed() {
+    console.log("02-UserName-销毁");
+  },
+};
+</script>
+
+<style>
+</style>
