ee84c5ceb3ff11eeb5ad8235281faa8c6e208f2c
‘98hb <w17339377595@163.com>
2022-02-13 12:02:04 +0800

    组件插槽

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,20 @@
     <hr />
     <h1>2. 组件的缓存</h1>
     <UseDynamic2></UseDynamic2>
+    <h1>3.组件-插槽</h1>
+    <UserSlot></UserSlot>
   </div>
 </template>
 
 <script>
 import UseDynamic from "./views/01_UseDynamic.vue";
 import UseDynamic2 from "./views/02_UseDynamic.vue";
+import UserSlot from "./views/03_UserSlot.vue";
 export default {
   components: {
     UseDynamic,
     UseDynamic2,
+    UserSlot,
   },
 };
 </script>
