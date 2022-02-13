0836ade504dbbfb9786a839889a2e5800c6c953d
‘98hb <w17339377595@163.com>
2022-02-13 20:10:59 +0800

    作用域插槽灵活使用

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,9 @@
     <hr />
     <h1>5.组件-作用域插槽</h1>
     <UserSlot3></UserSlot3>
+    <hr />
+    <h1>6.组件-作用域插槽使用场景</h1>
+    <UseTable></UseTable>
   </div>
 </template>
 
@@ -22,6 +25,7 @@ import UseDynamic2 from "./views/02_UseDynamic.vue";
 import UserSlot from "./views/03_UserSlot.vue";
 import UserSlot2 from "./views/04_UserSlot.vue";
 import UserSlot3 from "./views/05_UserSlot.vue";
+import UseTable from "./views/06_UseTable.vue";
 export default {
   components: {
     UseDynamic,
@@ -29,6 +33,7 @@ export default {
     UserSlot,
     UserSlot2,
     UserSlot3,
+    UseTable,
   },
 };
 </script>
